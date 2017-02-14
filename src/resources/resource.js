e07bc655617bda3a4932bfb22efb12cc66beeecc
John D'Agostino <john.dagostino@gmail.com>
2017-02-14 11:01:20 +1100

    correct references to path

--- src/resources/resource.js
+++ src/resources/resource.js
@@ -14,23 +14,23 @@ export default function resource(path) {
 
       findAll(query = {}) {
         const q = Object.assign({}, defaultFilter, query);
-        client.logger.trace('findAll', { path, query: q });
+        client.logger.trace('findAll', { path: this.path, query: q });
         return client.get(this.path, { query: q });
       },
       findOne(id) {
-        client.logger.trace('findOne', { path, id });
-        return client.get(url.resolve(path `/${id}`));
+        client.logger.trace('findOne', { path: this.path, id });
+        return client.get(url.resolve(path, `/${id}`));
       },
 
       get: (...args) => this.findOne(...args),
 
       create(params = {}) {
-        client.logger.trace('create', { path, params });
+        client.logger.trace('create', { path: this.path, params });
         return client.post(this.path, { body: params });
       },
       update(id, params = {}) {
-        client.logger.trace('update', { path, id, params });
-        return client.put(url.resolve(path `/${id}`), { body: params });
+        client.logger.trace('update', { path: this.path, id, params });
+        return client.put(url.resolve(path, `/${id}`), { body: params });
       },
     }
   );
