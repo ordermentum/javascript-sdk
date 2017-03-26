e309a5fbfbb299c26a4be7f4c60abc0064a15e9e
John D'Agostino <john@ordermentum.com>
2017-03-27 08:15:17 +1100

    remove null logger, use package

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
-import Client from './client';
-import NULL_LOGGER from './logger';
+import NULL_LOGGER from 'null-logger';
 
+import Client from './client';
 import resources from './resources';
 
-function createClient({ apiBase = 'https://app.ordermentum.com', timeout = 3000, token, logger = NULL_LOGGER }) {
+function createClient({
+  apiBase = 'https://app.ordermentum.com',
+  timeout = 3000,
+  token,
+  logger = NULL_LOGGER,
+}) {
   const client = new Client({ token, apiBase, timeout, logger });
 
   logger.info({ token, apiBase, timeout });
